b001d7132bb46a1b3ffd18efb099eb5fae5e6f5c
richCode-Ama <richard.appiah@amalitech.com>
2023-12-22 23:19:04 +0000

    .

---
+++ lib/widgets/common/icon_indicator.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+class IconIndicator extends StatelessWidget {
+  const IconIndicator(
+      {super.key,
+      // this.color = grey,
+      this.secondaryColor,
+      required this.leading,
+      this.trailing,
+      required this.text});
+  // final Color color;
+  final Color? secondaryColor;
+  final IconData leading;
+  final IconData? trailing;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          leading,
+          color: Theme.of(context).primaryColor
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Text(
+          text,
+          style: Theme.of(context)
+              .textTheme
+              .bodySmall
+              ?.copyWith(color: secondaryColor),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        trailing == null
+            ? const SizedBox.shrink()
+            : Icon(
+                Icons.arrow_downward,
+                color: Theme.of(context).primaryColor,
+              )
+      ],
+    );
+  }
+}
