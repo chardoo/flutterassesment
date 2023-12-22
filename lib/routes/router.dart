c43c5588667f9f0ed4861894f54db67613ea9f3c
richCode-Ama <richard.appiah@amalitech.com>
2023-12-22 23:38:50 +0000

    adding routes

---
+++ lib/routes/router.dart
@@ -0,0 +1,15 @@
+
+import 'package:get/get.dart';
+import 'package:test_app/screens/video_detail.dart';
+
+abstract class Routes {
+  static const SPLASH_ROUTE = "/";
+}
+
+abstract class AppPages {
+  static final List<GetPage> appPages = [
+    
+   
+   
+  ];
+}
