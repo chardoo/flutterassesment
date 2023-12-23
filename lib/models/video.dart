645aabfcadaabe6740e4dda40f621f09df1448b7
richCode-Ama <richard.appiah@amalitech.com>
2023-12-23 23:48:17 +0000

    adding islike attribute to video model

--- lib/models/video.dart
+++ lib/models/video.dart
@@ -3,11 +3,12 @@ import 'package:uuid/uuid.dart';
 class VideoModel {
   final String id;
   late int commentCount;
-  late  int favoriteCount;
+  late int favoriteCount;
   final DateTime postedOn;
   final String videoUrl;
   final String heading;
   final String description;
+  late bool isLike;
 
   VideoModel({
     required this.id,
@@ -17,10 +18,12 @@ class VideoModel {
     required this.videoUrl,
     required this.heading,
     required this.description,
+    required this.isLike,
   });
 
   factory VideoModel.fromJson(Map<String, dynamic> json) {
     return VideoModel(
+      isLike: json['isLike'],
       id: json['id'],
       commentCount: json['commentCount'] ?? 0,
       favoriteCount: json['favoriteCount'] ?? 0,
@@ -33,6 +36,6 @@ class VideoModel {
   @override
   String toString() {
     return 'VideoModel(  id: $id, commentCount: $commentCount, favoriteCount: $favoriteCount)';
-;
+    ;
   }
 }
