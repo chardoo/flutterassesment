00ed93203fdbdc3a27afd281cdca8f5baf857f74
richCode-Ama <richard.appiah@amalitech.com>
2023-12-24 20:31:20 +0000

    adding a commnet

--- lib/controller/comment_controller.dart
+++ lib/controller/comment_controller.dart
@@ -1,3 +1,4 @@
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:get/get_utils/src/platform/platform_web.dart';
 import 'package:test_app/models/comment.dart';
@@ -39,7 +40,7 @@ class CommentController extends GetxController {
               "Totally agree! I'm already feeling more confident about the material. ",
           postedOn: DateTime.now(),
         ),
-         Reply(
+        Reply(
           id: "fkdfdfkdfkdfdffd434",
           profileUrl:
               "https://images.pexels.com/photos/9559725/pexels-photo-9559725.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -48,7 +49,7 @@ class CommentController extends GetxController {
               " agree! I'm already feeling more smart about the material. ",
           postedOn: DateTime.now(),
         ),
-         Reply(
+        Reply(
           id: "fkdfdfkdfkdfdffd434",
           profileUrl:
               "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
@@ -68,10 +69,21 @@ class CommentController extends GetxController {
           "That guitar riff in the background is ! Anyone know the song name?",
       postedOn: DateTime.now(),
     ),
-  ];
+  ].cast<CommentModel>().obs;
 
-  Future<List<CommentModel>> getComment(String videoId) async{
-    return  comments;
+  Future<List<CommentModel>> getComment(String videoId) async {
+    return comments;
   }
-  
+
+  addComment(String message) {
+    CommentModel comment = CommentModel(
+        id: "43jjerje",
+        profileUrl:
+            "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=800",
+        handleName: "handleName",
+        message: message,
+        postedOn: DateTime.now());
+    comments.add(comment);
+    update();
+  }
 }
